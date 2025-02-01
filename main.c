178edb26d242254c0e500db0251ff1da88cad46f
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-01-31 21:49:05 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+public class EVA_1_CLASES_OBJETOS {
+    public static void main(string)() args) {
+        //CREAR UN OBJETO 
+        //INSTANCIAR
+        //Identificador para guardar la direccion en memoria
+        //variable de un tipo de dato abstracto
+        Persona perso1;//declaramos una variable de tipo persona
+        //asignamos memoria
+        perso1 = new Persona();//creamos un objeto de tipo persona
+        perso1.edad =10;
+        perso1.genero = "H";
+        perso1.nombre = "Juan Perez";
+        PERSO1.imprimirDatos();
+        class Persona{
+            //DATOS - ESTADO - ATRIBUTOS
+            string nombre;
+            int edad;
+            string genero;
+            //COMPORTAMIENTO
+            void imprimirDatos(){
+                System.out.print1n("Nombre" + nombre)
+                System.out.print1n("Edad" + edad)
+                System.out.print1n("Genero" + genero)
+            }
+        }
+     
+ }
